21e73e406477093879c6248c1ab819443430e70e
Lerian Febriana <kanglerian@MJLQ2s-MacBook-Pro.local>
2023-06-16 16:04:09 +0700

    data

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ app.get('/', (req, res) => {
 app.get('/books', async (req, res) => {
   try {
     const books = await Model.Books.findAll();
-    return res.json(books);
+    return res.json({
+      data: books
+    });
   } catch (error) {
     return res.json(error)
   }
